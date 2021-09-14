e7169ca3f554d0ecbd0411e4bcc53712bdfe55f8
Eugene Pavlenko <Eugene Pavlenko@RaymondJames.com>
2021-09-14 15:41:55 -0600

    Adding Resource Leak code and Deadlock code.

---
+++ src/main/java/com/evpa/DeadlockExample.java
@@ -0,0 +1,39 @@
+package com.evpa;
+
+public class DeadlockExample {
+
+    private Object process = new Object();
+    private boolean stopped = true;
+
+    public void run() {
+        synchronized (process) {
+            isStopped();
+        }
+    }
+
+    private synchronized boolean isStopped() {
+        return stopped;
+    }
+
+    public synchronized void stopProcess() {
+        synchronized (process) {
+            System.out.println("Stopping...");
+        }
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+
+        DeadlockExample deadlock = new DeadlockExample();
+
+        Thread threadA = new Thread(() -> {
+            deadlock.run();
+            System.out.println("Running...");
+        });
+        Thread threadB = new Thread(() -> {
+            deadlock.stopProcess();
+            System.out.println("Stopped...");
+        });
+        threadA.start();
+        threadB.start();
+    }
+}
